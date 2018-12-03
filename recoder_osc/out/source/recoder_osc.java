37ad8e729be3dcc1acabd073bc0c052cf81618ed
axis06 <withcomputer@gmail.com>
2018-12-03 18:25:17 +0900

    [fix] log

--- recoder_osc/out/source/recoder_osc.java
+++ recoder_osc/out/source/recoder_osc.java
@@ -22,14 +22,25 @@ public class recoder_osc extends PApplet {
  
 OscP5 oscP5;
 NetAddress myRemoteLocation;
- 
+PrintWriter file;
+
+boolean debug = false;
+
 public void setup(){
   
   oscP5 = new OscP5(this, 12000);
   myRemoteLocation = new NetAddress("127.0.0.1", 12000);
+
+  int s = second();
+  int m = minute();
+  int h = hour();
+  String t = h + nf(m, 2) + nf(s, 2);
+
+  String fileName = createFileName();
+  file = createWriter(fileName);
 }
  
-public void draw(){
+public void draw(){ 
 
 }
  
@@ -38,9 +49,26 @@ public void oscEvent(OscMessage theOscMessage){
   float oneValue = theOscMessage.get(0).floatValue();
   float twoValue = theOscMessage.get(1).floatValue();
   println("OSC Message Recieved");
-  println(oneValue);
-  println(twoValue);
+  if(debug){
+    println(oneValue);
+    println(twoValue);
+  }
+  file.print(oneValue + "," + twoValue);
 }
+
+
+public String createFileName() {
+  String fileName = "log/";
+  fileName += nf(year(), 2) + nf(month(), 2) + nf(day(), 2) +"-"+ nf(hour(), 2) + nf(minute(), 2) + nf(second(), 2);
+  fileName += ".csv";
+  return fileName;
+}
+
+ public void dispose() {
+  file.flush();
+  file.close();
+  exit();
+ }
   public void settings() {  size(120, 120); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "recoder_osc" };
--- recoder_osc/recoder_osc.pde
+++ recoder_osc/recoder_osc.pde
@@ -3,14 +3,25 @@ import netP5.*;
  
 OscP5 oscP5;
 NetAddress myRemoteLocation;
- 
+PrintWriter file;
+
+boolean debug = false;
+
 void setup(){
   size(120, 120);
   oscP5 = new OscP5(this, 12000);
   myRemoteLocation = new NetAddress("127.0.0.1", 12000);
+
+  int s = second();
+  int m = minute();
+  int h = hour();
+  String t = h + nf(m, 2) + nf(s, 2);
+
+  String fileName = createFileName();
+  file = createWriter(fileName);
 }
  
-void draw(){
+void draw(){ 
 
 }
  
@@ -19,6 +30,23 @@ void oscEvent(OscMessage theOscMessage){
   float oneValue = theOscMessage.get(0).floatValue();
   float twoValue = theOscMessage.get(1).floatValue();
   println("OSC Message Recieved");
-  println(oneValue);
-  println(twoValue);
+  if(debug){
+    println(oneValue);
+    println(twoValue);
+  }
+  file.print(oneValue + "," + twoValue);
 }
+
+
+String createFileName() {
+  String fileName = "log/";
+  fileName += nf(year(), 2) + nf(month(), 2) + nf(day(), 2) +"-"+ nf(hour(), 2) + nf(minute(), 2) + nf(second(), 2);
+  fileName += ".csv";
+  return fileName;
+}
+
+ void dispose() {
+  file.flush();
+  file.close();
+  exit();
+ }
